85f37c2e864ff8611c54c8e00a9ef9c91774a881
TanyaShtatnykh <tanya.shtatnykh@gmail.com>
2023-07-24 00:57:57 +0700

    fixed 2

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -3,6 +3,43 @@
 
 
 
+
+$(function () {
+  $('.welcome__appointment').on('click', function () {
+    $('.popup').addClass('popup--open');
+  });
+
+  $('.option__btn').on('click', function () {
+    $('.popup').addClass('popup--open');
+  });
+  
+  $('.popup__close').on('click', function () {
+    $('.popup').removeClass('popup--open');
+  })
+});
+$(function () {
+  $('.reviews__slides').slick({
+    dots: true,
+    arrows: false,
+    adaptiveHeight: true
+  });
+});
+$(window).on('load resize', function() {
+    if ($(window).width() < 992) {
+      $('.welcome__photo').insertAfter('.welcome__social');
+    };
+  });
+$(function() {
+  $('#tabs').tabs({
+  });
+});
+$(function () {
+    $('.price__list').slick({
+      dots: true,
+      arrows: false,
+      adaptiveHeight: true
+    });
+  });
 $(function () {
     $('.header__burger-btn').on('click', function() {
       $(this).toggleClass('header__burger-btn--open');
@@ -19,41 +56,4 @@ $(function () {
         })
       };
     });
-  });
-$(function() {
-  $('#tabs').tabs({
-  });
-});
-
-$(function () {
-  $('.welcome__appointment').on('click', function () {
-    $('.popup').addClass('popup--open');
-  });
-
-  $('.option__btn').on('click', function () {
-    $('.popup').addClass('popup--open');
-  });
-  
-  $('.popup__close').on('click', function () {
-    $('.popup').removeClass('popup--open');
-  })
-});
-$(function () {
-    $('.price__list').slick({
-      dots: true,
-      arrows: false,
-      adaptiveHeight: true
-    });
-  });
-$(function () {
-  $('.reviews__slides').slick({
-    dots: true,
-    arrows: false,
-    adaptiveHeight: true
-  });
-});
-$(window).on('load resize', function() {
-    if ($(window).width() < 992) {
-      $('.welcome__photo').insertAfter('.welcome__social');
-    };
   });
